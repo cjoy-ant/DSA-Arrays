923eb0dddaddbba4f9b461373ec43e01efcbb09f
Christine Antonio <christinejm.antonio@gmail.com>
2021-05-10 12:07:14 -0700

    maxSum

---
+++ maxSum.js
@@ -0,0 +1,13 @@
+const maxSum = (array) => {
+  let currentSum = 0
+  let largestSum = 0
+
+  for(let num of array){
+      currentSum = Math.max(0, (currentSum + num))
+      largestSum = Math.max(largestSum, currentSum)
+  }
+  
+  return largestSum
+
+}
+module.exports = maxSum
